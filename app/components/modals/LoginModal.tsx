00afb4cb44dd530ecfa7d91ed0d9fec40673e908
Shauin Mac <shinjitsukasa09@gmail.com>
2024-01-01 09:10:57 +0800

    good nice update

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn } from "next-auth/react"
+import { signIn } from "next-auth/react";
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
@@ -18,8 +18,8 @@ import { useRouter } from "next/navigation";
 
 const LoginModal = () => {
   const router = useRouter();
-  const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -36,15 +36,14 @@ const LoginModal = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    signIn('credentials', {
+    signIn("credentials", {
       ...data,
       redirect: false,
-    })
-    .then((callback) => {
+    }).then((callback) => {
       setIsLoading(false);
 
       if (callback?.ok) {
-        toast.success('Logged in');
+        toast.success("Logged in");
         router.refresh();
         loginModal.onClose();
       }
@@ -52,12 +51,17 @@ const LoginModal = () => {
       if (callback?.error) {
         toast.error(callback.error);
       }
-    })
+    });
   };
+
+  const onToggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="WELCOME BACK" subtitle="Login to your account" />
+      <Heading title="Welcome back" subtitle="Login to your account!" />
       <Input
         id="email"
         label="Email"
@@ -68,8 +72,8 @@ const LoginModal = () => {
       />
       <Input
         id="password"
-        type="password"
         label="Password"
+        type="password"
         disabled={isLoading}
         register={register}
         errors={errors}
@@ -85,38 +89,32 @@ const LoginModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
-
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div
         className="
-       text-neutral-500
-       text-center
-       mt-4
-       font-light
-       "
+      text-neutral-500 text-center mt-4 font-light"
       >
-        <div
-          className="
-            justify-center flex flex-row items-center gap-2"
-        >
-          <div>Already have an account?</div>
-          <div
-            onClick={loginModal.onClose}
+        <p>
+          First time using Airbnb?
+          <span
+            onClick={onToggle}
             className="
-            text-neutral-800
-            cursor-pointer
-            hover:underline"
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
           >
-            Log in
-          </div>
-        </div>
+            {" "}
+            Create an account
+          </span>
+        </p>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./components/providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUsers";
 
 const inter = Nunito({ subsets: ["latin"] });
 
@@ -14,11 +15,13 @@ export const metadata: Metadata = {
   description: "Airbnb clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -26,7 +29,7 @@ export default function RootLayout({
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
