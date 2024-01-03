86708a4c307a89b3db29165c6a6340503e208537
Shauin Mac <shinjitsukasa09@gmail.com>
2024-01-03 08:03:44 +0800

    updates category

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,5 +1,30 @@
 import Container from "../Container";
+import { TbBeach } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
 
+export const categories = [
+	{
+		label: 'Beach',
+		icon: TbBeach,
+		description: 'This property is close to the beach!'
+	},
+	{
+		label: 'Windmillls',
+		icon: GiWindmill,
+		description: 'This property has windmill!',
+	},
+	{
+		label: 'Modern',
+		icon: MdOutlineVilla,
+		description: 'This property is modern!',
+	},
+	// {
+	// 	label: 'Windmillls',
+	// 	icon: GiWindmill,
+	// 	description: 'This property has windmill!',
+	// },
+]
 
 const Categories = () => {
 	return ( 
